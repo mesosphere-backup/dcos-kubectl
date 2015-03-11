76e441d1df4c0bac6068f1427d1033072897c23a
José Armando García Sancio <jsancio@gmail.com>
2015-03-11 08:27:56 -0700

    Add integration script

---
+++ bin/start_integration_test.sh
@@ -0,0 +1,9 @@
+# Clean and recreate environment
+cd /dcos-helloworld
+make clean env
+
+# Activate the virtual environment so that we can run make
+source env/bin/activate
+
+# Run the default target: E.g. test and packages
+make

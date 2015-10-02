e39d3b0589e20e55e942354c2e26e69b9e471f4f
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2015-10-02 14:34:59 +0200

    Fix download chunk number

--- dcos_kubernetes/cli.py
+++ dcos_kubernetes/cli.py
@@ -40,8 +40,8 @@ def download_kubectl(url, kubectl_path):
             import urllib2
             f = urllib2.urlopen(url)
             total_length = int(f.info().getheader("Content-Length"))
-            chunk_size = (total_length/1024) + 1
-            for chunk in progress.bar(read_in_chunks(f, chunk_size=chunk_size), expected_size=chunk_size):
+            chunk_num = int(total_length/1024) + 1
+            for chunk in progress.bar(read_in_chunks(f, chunk_size=1024), expected_size=chunk_num):
                 if chunk:
                     os.write(fd, chunk)
 

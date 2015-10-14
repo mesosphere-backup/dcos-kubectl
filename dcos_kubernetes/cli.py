85d1140efab1078027c00a0f824b4bf96f0978fb
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2015-10-14 18:43:37 +0200

    Integrate with kubernetes-mesos image to download kubectl

--- dcos_kubernetes/cli.py
+++ dcos_kubernetes/cli.py
@@ -9,33 +9,30 @@ from dcos.subcommand import package_dir
 
 def kubectl_binary_path_and_url(master):
     # get kubectl meta json
-    import requests, json
-    meta = requests.get(master + "/static/kubectl-meta.json")
-    meta_json = json.loads(meta)
+    import requests
+    meta_url = master + "/static/kubectl-meta.json"
+    try:
+        meta = requests.get(meta_url).json()
+    except:
+        raise Exception("Cannot download kubectl meta information from {0}".format(meta_url))
 
     # get url
     system, node, release, version, machine, processor = platform.uname()
-    if system == "Linux":
-        os = "linux"
-    elif system == "Darwin":
-        os = "darwin"
-    elif system == "Windows":
-        os = "windows"
     arch = "amd64" # we only support amd64 for the moment
-    key = os + "-" + arch
-    if key not in meta_json:
-        raise Exception("System type %1 not supported".format(key))
-    url = master + "/static/" + meta_json[key].file
-    hash = meta_json[key].sha256
+    key = system.lower() + "-" + arch
+    if key not in meta:
+        raise Exception("System type {0} not supported".format(key))
+    url = master + "/static/" + meta[key]["file"] + ".bz2"
+    hash = meta[key]["sha256"]
 
     # create filename
     data_dir = package_dir("kubernetes")
     base = os.path.join(data_dir, "kubectl")
-    file = base + "-" + hash
+    file_path = base + "-" + hash
     if system == "Windows":
-        file += ".exe"
+        file_path += ".exe"
 
-    return file, url
+    return file_path, url
 
 def read_in_chunks(file_object, chunk_size=1024):
     while True:
@@ -44,35 +41,28 @@ def read_in_chunks(file_object, chunk_size=1024):
             break
         yield data
 
-
 def download_kubectl(url, kubectl_path):
     import tarfile
 
-    with dcos.util.temptext() as (fd, tar_file_path):
+    with dcos.util.temptext() as (fd, file_path):
         try:
-            # download tar.gz
+            # download bz2 file and decompress in time
             print "Download kubectl from " + url
             from clint.textui import progress
-            import urllib2
+            import urllib2, bz2
             f = urllib2.urlopen(url)
+            decompressor = bz2.BZ2Decompressor()
             total_length = int(f.info().getheader("Content-Length"))
             chunk_num = int(total_length/1024) + 1
             chunks = read_in_chunks(f, chunk_size=1024)
             for chunk in progress.bar(chunks, expected_size=chunk_num):
                 if chunk:
-                    os.write(fd, chunk)
+                    os.write(fd, decompressor.decompress(chunk))
 
-            # unarchive kubectl from tar.gz
-            tar_file = tarfile.open(tar_file_path, mode="r:gz")
-            tar_file.getmembers()
-            kubectl_tar_file = tar_file.extractfile("kubectl")
-            kubectl_file = open(kubectl_path, 'wb')
-            kubectl_file.write(kubectl_tar_file.read())
-            kubectl_tar_file.close()
-            tar_file.close()
-
-            # make executable
-            os.chmod(kubectl_path, 0755)
+            # move binary at right spot and make executable
+            os.rename(file_path, kubectl_path)
+            if not kubectl_path.endswith(".exe"):
+                os.chmod(kubectl_path, 0755)
 
         except tarfile.TarError, e:
             os.unlink(kubectl_path)
@@ -106,11 +96,13 @@ def main():
     # check whether kubectl binary exists and download if not
     import urlparse
     master = urlparse.urljoin(dcos_url, "service/kubernetes")
-    kubectl_path, kubectl_url = kubectl_binary_path_and_url(master)
-    if kubectl_path is None:
-        print("Error: unsupported operating system")
+    try:
+        kubectl_path, kubectl_url = kubectl_binary_path_and_url(master)
+    except Exception, e:
+        print("Error: " + str(e))
         return 2
     if not os.path.exists(os.path.dirname(kubectl_path)):
+        print "create"
         os.makedirs(os.path.dirname(kubectl_path))
     if not os.path.exists(kubectl_path):
         download_kubectl(kubectl_url, kubectl_path)
